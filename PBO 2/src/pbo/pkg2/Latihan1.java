4a9fb8c5e8076a8dcb2c50feb57ed126552d19d5
LindaJuliana <117423038+LindaJuliana@users.noreply.github.com>
2022-11-05 10:17:06 +0700

    Add files via upload

---
+++ PBO 2/src/pbo/pkg2/Latihan1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pbo.pkg2;
+
+/**
+ *
+ * @author ASUS-E402Y
+ */
+public class Latihan1 {
+    public class Penjumlahan {
+        public static void main (String[] args) {
+            int nilaiA = 1500;
+            int nilaiB = 75;
+            int jumlah = 1575;
+            // Lengkapilah kode pada baris  ini
+            System.out.println("Nilai A = " + nilaiA);
+            System.out.println("Nilai B = " + nilaiB);
+            System.out.println("Jumlah = " + jumlah);
+        }
+    }
+    
+}
